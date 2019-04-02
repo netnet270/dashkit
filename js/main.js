2fd74cb54e651882e583c55c6a51589b64e09002
nguyen.thi.net <FRAMGIA\nguyen.thi.net@b121787.framgia.com>
2019-04-02 17:07:02 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   $('.goal-table').DataTable();
-  menu();
+
+  $('[data-toggle="tooltip"]').tooltip();  
+
+  // menu();
   chartOrder();
   chartDevices();
   chartPerformance(); 
@@ -14,88 +17,137 @@ $(document).ready(function () {
       if($(this).hasClass('navbar-link--active')){
         $(this).removeClass('navbar-link--active');
       }
+      
       else{
         $('.js-navbar-link').removeClass('navbar-link--active')
-        $('.js-submenu').slideUp(300);
-        menuItem.children('.js-submenu').slideDown(400);
+        $('.collapse').removeClass('show');
         $(this).addClass('navbar-link--active');
       } 
     })
   }
   
   function chartOrder(){
-    var ctx = $('#orderChart');
-    var chart = new Chart(ctx, {
-      type: 'bar',
-      options: {
-        tooltips: {
-         
-          callbacks: {
-            title: function(tooltipItem, data) {
-              return data['labels'][tooltipItem[0]['index']];
-            },  
-            label: function(tooltipItem, data) {
-              return 0;
-            },
-
-            afterLabel: function(tooltipItem, data) {
-              var dataset = data['datasets'][0];
-              var percent = Math.round((dataset['data'][tooltipItem['index']] / dataset["_meta"][0]['total']) * 100)
-              return  percent + '%';
-            }
+    var _chart1;
+    function drawOrder(_data) {
+      _chart1 = new Chart(document.getElementById('orderChart'), {
+        type: "bar",
+        data: _data,
+        options: {
+          tooltips: {
+            enabled: false,
+            mode: "index",
+            intersect: false,
+            custom: function (r) {
+              var a = $("#chart-tooltip");
+              if (a.length || (a = $('<div id="chart-tooltip" class="popover bs-popover-top" role="tooltip"></div>'), $("body").append(a)), 0 !== r.opacity) {
+                if (r.body) {
+                  var e = r.title || [],
+                    l = r.body.map(function (a) {
+                      return a.lines
+                    }),
+                    n = "";
+                  n += '<div class="arrow"></div>', e.forEach(function (a) {
+                    n += '<h3 class="popover-header text-center">' + a + "</h3>"
+                }), l.forEach(function (a, e) {
+                  var t = '<span class="popover-body-indicator" style="background-color: ' + r.labelColors[e].backgroundColor + '"></span>',
+                    o = 1 < l.length ? "justify-content-left" : "justify-content-center";
+                  n += '<div class="popover-body d-flex align-items-center ' + o + '">' + t + a + "</div>"
+                }), a.html(n)
+              }
+              var t = $(this._chart.canvas),
+                o = (t.outerWidth(), t.outerHeight(), t.offset().top),
+                s = t.offset().left,
+                i = a.outerWidth(),
+                c = a.outerHeight(),
+                d = o + r.caretY - c - 16,
+                u = s + r.caretX - i / 2;
+              a.css({
+                top: "calc(" + d + "px - 1.5%)",
+                left: u + "px",
+                display: "block"
+              })
+            } else a.css("display", "none")
           },
-
-          backgroundColor: '#FFF',
-          titleFontSize: 16,
-          titleFontColor: '#0066ff',
-          bodyFontColor: '#000',
-          bodyFontSize: 14,
-          borderWidth: 1,
-          borderColor: '#ccc',
-          displayColors: false
+          callbacks: {
+            label: function (a, e) {
+              var t = e.datasets[a.datasetIndex].label || "",
+                o = a.yLabel,
+                r = "";
+              return 1 < e.datasets.length && (r += '<span class="popover-body-label mr-auto">' + t + "</span>"), r += '<span class="popover-body-value">$' + o + "k</span>"
+            },
+            labelColor: function (tooltipItem, chart) {
+              return {
+                backgroundColor: '#2C7BE5'
+              };
+            }
+          }
         },
-
-        legend: {
-          display:false
-        },
-
         scales: {
           xAxes: [{
-            barPercentage: 0.5,
-            barThickness: 6,
-            maxBarThickness: 8,
-            categoryPercentage: 9,
-            minBarLength: 2,
+            barThickness: 10,
+            ticks: {
+              fontWeight: 700,
+              fontSize: 13,
+              fontColor: '#90a0b7',
+            },
             gridLines: {
-              offsetGridLines: false,
-              color: "#fff"
+              display: false,
             }
           }],
           yAxes: [{
             ticks: {
+              fontWeight: 700,
+              fontSize: 14,
+              fontColor: '#90a0b7',
               beginAtZero: true,
-              callback: function(ctx) {
-                if (!(ctx % 10)) return "$" + ctx + "k"
+              callback: function (a) {
+                if (!(a % 10)) return "$" + a + "k"
               }
+            },
+            gridLines: {
+              drawBorder: false,
+              borderDash: [2, 4],
+              color: "#e6edf7"
             }
           }]
-        }
-      },
-      data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [{
-          label: "Sales",
-          data: [25, 20, 30, 22, 17, 10, 18, 26, 28, 26, 20, 32],
-          backgroundColor:"#2d7be5",
         },
-        {
-          label: "Affiliates",
-          data: [25, 20, 15, 22, 17, 10, 18, 26, 28, 26, 20, 32],
-          backgroundColor:"#000",
-        }]
+        legend: {
+          display: false,
+        }
       }
-    
     })
+  }
+
+  var tooltip = $('[data-toggle="tooltip"]');
+  tooltip.length && tooltip.tooltip();
+
+  var dataOrder = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: "Sales",
+      data: [25, 20, 30, 22, 17, 10, 18, 26, 28, 26, 20, 32],
+      backgroundColor: "#2c7be5",
+      borderColor: "#2c7be5",
+    }]
+  }
+
+  drawOrder(dataOrder);
+
+  $("#cardToggle").change(function (e) {
+    if ($(this).is(':checked')) {
+      dataOrder.datasets.push(
+        {
+          data: [15, 10, 20, 12, 7, 0, 8, 16, 18, 16, 10, 22],
+          backgroundColor: "#d2ddec",
+          label: "Affiliate"
+        }
+      )
+    } else {
+      dataOrder.datasets.splice(-1, 1);
+    }
+    _chart1.update();
+  });
+
   }
 
   function chartDevices(){
@@ -111,7 +163,6 @@ $(document).ready(function () {
             datasets: [{
               data: parseDataset['datasets'],
               backgroundColor: ["#2d7be5" ,"#a6c5f7" ,"#d2ddec"],
-              hoverBorderColor: "transparent"
             }]
           },
   
@@ -121,56 +172,70 @@ $(document).ready(function () {
               display:false
             },
             tooltips: {
-              callbacks: {
-                title: function(a, e) {
-                  return e.labels[a[0].index]
-                },
-                label: function(a, e) {
-                  var t = "";
-                  return t += e.datasets[0].data[a.index] + "%"
-                }
-              }
+              
             }
           }
       });
     })
+  
   }
 
   function chartPerformance(){
-    $('.js-performance-chart').each(function(){
-      dataSet = $(this).attr("datasets");
-      parseDataset = JSON.parse(dataSet); 
-
-      var chart = new Chart( $(this), {
-        type: "line",
+    var ctx = document.getElementById('performanceChart').getContext('2d');
+    
+    var chart = new Chart( ctx, {
+      type: 'line',
         options: {
+          elements: {
+            point:{
+              radius: 0
+            },
+        },
+          legend: {
+            display: false,
+          },
           scales: {
             yAxes: [{
+              gridLines: {
+                drawBorder: false,
+                borderDash: [2, 2],
+              },
               ticks: {
-                callback: function(a) {
-                    if (!(a % 10)) return "$" + a + "k"
-                }
+                beginAtZero: true,
+                callback: function(ctx) {
+                  if (!(ctx % 10)) return "$" +" " + ctx + "k"
+                },
+                fontColor: "#a9afb9",
+                scaleFontSize: 20
               }
-            }]
-          },
-          tooltips: {
-            callbacks: {
-              label: function(a, e) {
-                var t = e.datasets[a.datasetIndex].label || "",
-                  o = a.yLabel,
-                  r = "";
-                return 1 < e.datasets.length && (r += '<span class="popover-body-label mr-auto">' + t + "</span>"), r += '<span class="popover-body-value">$' + o + "k</span>"
-              }
-            }
+            }],
+            xAxes: [{
+              barPercentage: 0.5,
+              barThickness: 30,
+              maxBarThickness: 10,
+              categoryPercentage: 9,
+              minBarLength: 2,
+              gridLines: {
+                offsetGridLines: false,
+                color: "#fff",
+              },
+              ticks: {
+                fontColor: "#a9afb9",
+                fontFamily: "Cerebri Sans",
+                scaleFontSize: 20,
+                lineDashType: "dash"
+            },
+            }],
           }
         },
         data: {
           labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
           datasets: [{
-              label: "Performance",
-              data: [0, 10, 5, 15, 10, 20, 15, 25, 20, 30, 25, 40]
+            label: "Performance",
+            fill : false,
+            borderColor : "#2c7be5",
+            data: [0, 10, 5, 15, 10, 20, 15, 25, 20, 30, 25, 40]
           }]
         }
-      });
-    })
+    });
   }
